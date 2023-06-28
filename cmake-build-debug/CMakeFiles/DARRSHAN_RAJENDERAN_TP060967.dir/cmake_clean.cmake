940fec9c04c77fe1d23bf50330f4db1ab59754e7
Darrshan Rajenderan <85858497+Fedwig@users.noreply.github.com>
2023-06-29 00:50:29 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/DARRSHAN_RAJENDERAN_TP060967.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DARRSHAN_RAJENDERAN_TP060967.dir/APIMS.c.obj"
+  "DARRSHAN_RAJENDERAN_TP060967.exe"
+  "DARRSHAN_RAJENDERAN_TP060967.exe.manifest"
+  "DARRSHAN_RAJENDERAN_TP060967.pdb"
+  "libDARRSHAN_RAJENDERAN_TP060967.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DARRSHAN_RAJENDERAN_TP060967.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
